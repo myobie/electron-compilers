12ac2159eef3a7a1a2192dc96d2d7ec7acf30cd9
myobie <me@nathanherald.com>
2016-07-29 19:00:07 +0200

    more logging

--- src/css/postcss.js
+++ src/css/postcss.js
@@ -19,6 +19,7 @@ export default class PostCSSCompiler extends CompilerBase {
     try {
       fs.statSync()
     } catch (e) {
+      console.error(e)
       possibleFile = null
     }
     if (possibleFile) {
